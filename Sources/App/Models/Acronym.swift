2462c9792d1663a8a6b3724c7f98210688245d31
Marek Galik <marek.galik@ibm.com>
2019-04-04 14:42:42 +0200

    Add sibling relationships

--- Sources/App/Models/Acronym.swift
+++ Sources/App/Models/Acronym.swift
@@ -31,4 +31,8 @@ extension Acronym: Migration {
             builder.reference(from: \.userID, to: \User.id)
         }
     }
+    
+    var categories: Siblings<Acronym, Category, AcronymCategoryPivot> {
+        return siblings()
+    }
 }
