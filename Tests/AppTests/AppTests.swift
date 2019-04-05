f42e5ca78c8b4ebe8f3ecad21600fc849506c322
Marek Galik <marek.galik@ibm.com>
2019-04-05 14:42:24 +0200

    Tests in the repository

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -1,13 +1,13 @@
 import App
 import XCTest
 
-final class AppTests: XCTestCase {
-    func testNothing() throws {
-        // Add your tests here
-        XCTAssert(true)
-    }
-
-    static let allTests = [
-        ("testNothing", testNothing)
-    ]
-}
+//final class AppTests: XCTestCase {
+//    func testNothing() throws {
+//        // Add your tests here
+//        XCTAssert(true)
+//    }
+//
+//    static let allTests = [
+//        ("testNothing", testNothing)
+//    ]
+//}
